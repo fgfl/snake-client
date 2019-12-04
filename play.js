37a73ff417f1c82057aa659cc8c7ede760663ae3
Frederick <frederick.gfl@gmail.com>
2019-12-04 23:12:58 +0000

    Handle user input
    add code to handle user input with stdin
    - add case to quit with ctrl+C.

--- play.js
+++ play.js
@@ -5,12 +5,31 @@
 const {connect} = require('./client');
 
 /**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = key => {
+  if (key === '\u0003')
+    process.exit();
+};
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput); 
+
+  return stdin;
+}
+
+/**
  * Establishes connection with the game server
  */
 
 const serverIP = '192.168.88.149';
 const serverPort = 50541;
-const name = 'FFL';
 
 const cmmds = {
   up: "Move: up"
@@ -18,3 +37,4 @@ const cmmds = {
 
 console.log('Connecting ....');
 const client = connect(serverIP, serverPort);
+const userIO = setupInput();
